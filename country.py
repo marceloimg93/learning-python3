6a749a4daade669bebd30382a0628c6ee8aca8f4
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-18 19:07:04 -0300

    allow user to input list of countries and list it

--- country.py
+++ country.py
@@ -1,8 +1,21 @@
-countries = ["brazil", "argentina"]
+countries = []
 
-country = input("Enter a country name: ").lower()
+print("Welcome to your visited countries list")
+print(f"So far you have visited {len(countries)} countries.")
 
-if country in countries:
-    print("We have visited that country")
-else :
-    print("We have NOT visited that country yet")
+user_input = ""
+
+while user_input != "Q":
+    user_input = input(
+        "Press Q to quit, A to add or L to list all countries: ")
+    if user_input == "A":
+        visited_country = input("Enter a country name: ").lower()
+
+        if visited_country in countries:
+            print("We have visited this country already")
+        else:
+            countries.append(visited_country)
+    elif user_input == "L":
+        print(f"You have visited {len(countries)} countries: ")
+        for country in countries:
+            print(f" ----> {country.upper()}")
