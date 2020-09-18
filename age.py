d919f6cf4807ea23ae1faff8ad4d94a613088279
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-18 16:06:15 -0300

    return age in days, hours and seconds

--- age.py
+++ age.py
@@ -4,6 +4,11 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 age_as_number = int(age)
 months = age_as_number * 12
-months_as_string = str(months)
+days = age_as_number * 365
+hours = days * 24
+seconds = hours * 60 * 60
 
-print(f"Hi {name} \nYour age in months is: {months}")
+print(f"Hi {name} \nYour age in MONTHS is: {months}")
+print(f"Your age is DAYS is approximately: {days}")
+print(f"Your age is HOURS is: {hours}")
+print(f"Your age is SECONDS is: {seconds}")
