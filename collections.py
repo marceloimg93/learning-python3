e22dc71d2b3ca35ff3601071da5e77d85bec8974
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-19 19:16:38 -0300

    advanced set operations

--- collections.py
+++ collections.py
@@ -20,5 +20,18 @@ print(l)
 
 # set use ADD command because append adds at the end of list while SET does not have an order, you must use add method
 s.add("John")
-s.add("John") # SET does not allow duplicated values
+s.add("John")  # SET does not allow duplicated values
 print(s)
+
+# advanced operations with set
+friends = {"John", "Smith", "Kevin"}
+abroad = {"John", "Kevin", "Robert"}
+
+local_friends = friends.difference(abroad)
+print(f"Your local friends are: {local_friends}")
+
+abroad_friends = friends.intersection(abroad)
+print(f"Friends that are abroad: {abroad_friends}")
+
+total_friends = friends.union(abroad)
+print(f"Friends Set Union With Abroad Set: {total_friends}")
