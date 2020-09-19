7210f31f23cc918a9b9714a74e3266da85016801
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-19 19:43:50 -0300

    add guess secret number game

--- in_keyword.py
+++ in_keyword.py
@@ -10,3 +10,18 @@ if user_movie in watched_movies:
     print("GREAT!!! I watched it too!")
 else:
     print("Cool, I never watched this movie before. I'll check it out!")
+
+secret_number = 7
+
+wants_to_play = input(
+    "Would you like to play the Guess Number game? (Press Y to Yes): ").lower()
+
+if wants_to_play == 'y':
+    user_number = int(input("Great! Guess a number: "))
+    if secret_number == user_number:
+        print("THAT IS AMAZING!!! YOU WON!!!")
+    # this could be replaced by: abs(user_number - secret_number) == 1, However the purpose of this section is to explore the IN keyword
+    elif user_number - secret_number in (1, -1):
+        print("WOWWW... so close!!! You missed by one number")
+    else:
+        print("Sorry, you did not win. =/")
