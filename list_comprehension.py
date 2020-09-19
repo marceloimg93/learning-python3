065aa71e8b30d4ac45abf03d1a5056e7f3c31222
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-19 20:09:23 -0300

    add list comprehension

---
+++ list_comprehension.py
@@ -0,0 +1,11 @@
+# Yes... this is AWESOME 
+
+numbers = [2, 10, 30]
+doubled = [x * 2 for x in numbers]
+
+print(doubled)
+
+friends = ["Samantha", "Robert", "John", "Samira", "Sam"]
+starts_s = [friend for friend in friends if friend.startswith('S')]
+
+print(starts_s)
