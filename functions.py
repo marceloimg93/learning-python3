527a7cac7fe025dd8c3e14eb8e333cb7b6e21f22
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-20 18:42:16 -0300

    playing arround with functions

---
+++ functions.py
@@ -0,0 +1,12 @@
+def say_hello(name, surname):
+    print(f'Hello {surname}, {name}!')
+
+
+say_hello(surname="Kevin", name="Bacon")
+
+
+def add_two_numbers(x, y):
+    return x + y
+
+numbers_added = add_two_numbers(2,3)
+print(numbers_added)
