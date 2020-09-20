9f2f4fd6300b84c3ddaf3f1238648c62236525fc
Marcelo Ivo <marceloimgontijo@gmail.com>
2020-09-20 18:57:02 -0300

    play with lambda functions

--- functions.py
+++ functions.py
@@ -8,5 +8,20 @@ say_hello(surname="Kevin", name="Bacon")
 def add_two_numbers(x, y):
     return x + y
 
-numbers_added = add_two_numbers(2,3)
+
+numbers_added = add_two_numbers(2, 3)
 print(numbers_added)
+
+# lambda functions - functions without a name, however you can name it by signing it to variables
+# lambda functions should not do complicated things because it can be hard to read
+# lambda functions should have only a single line of code
+def double(x):
+    return x * 2
+
+
+sequence = [2, 4, 5, 6, 10]
+doubled = [double(x) for x in sequence]
+doubled = list(map(double, sequence))  # it does the same thing as the above code
+doubled_lambda = list(map(lambda x: x * 2, sequence))
+print(doubled)
+print(doubled_lambda)
